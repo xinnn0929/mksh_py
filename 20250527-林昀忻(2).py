cf54263bccae33145842a1a202357b9a4f8659bf
xinnn0929 <kelly030308@gmail.com>
2025-05-27 15:16:14 +0800

    Add files via upload

---
+++ 20250527-林昀忻(2).py
@@ -0,0 +1,9 @@
+stu1 = ["王大明" , 174, 65]
+stu2 = ["林小華" , 168, 58]
+stu3 = ["許中和" , 182, 70]
+stu4 = ["趙小慧" , 159, 45]
+body_info = [stu1, stu2, stu3, stu4]
+for info in body_info:
+    for item in info:
+        print(item, " ", end="")
+    print()
